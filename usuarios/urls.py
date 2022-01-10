903f23707e2a87c3fb68bba67f9b04f79234a2c2
Italo-Neves <italorei7@gmail.com>
2022-01-10 14:25:00 -0300

    Adicao dos templates de cadastro/login e configuracao das urls

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from django.urls import path
-
 from . import views
 
-urlpattherns = [
-    path('cadastro',views.index, name='cadastro'),
+urlpatterns = [
+    path('cadastro',views.cadastro, name='cadastro'),
     path('login', views.login, name='login'),
     path('dashboard', views.dashboard, name='dashboard'),
     path('logout', views.logout, name='logout'),
